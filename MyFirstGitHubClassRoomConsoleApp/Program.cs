c7fabf963b55b2fa383b4617336addbf38f1f7a0
jannick <jannickpedersen@live.dk>
2019-02-11 10:03:34 +0100

    Nu er denne opgave løst med en writeline og class med proberties

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -12,6 +12,10 @@ namespace MyFirstGitHubClassRoomConsoleApp
         {
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
             //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
+
+            Console.WriteLine("Hej med dig");
+            Console.ReadLine();
+
         }
     }
 }
